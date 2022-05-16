AsafFisher/rustix

Fix several clippy lints.
Use fewer `as` casts, make sure that Safety comments for `unsafe` blocks
are properly formatted, and others.

src/imp/libc/fs/syscalls.rs
@@ -164,9 +164,9 @@ fn openat_via_syscall(
         let mode = c::c_uint::from(mode.bits());
         ret_owned_fd(c::syscall(
             c::SYS_openat,
-            dirfd as c::c_long,
+            c::c_long::from(dirfd),
             path,
-            oflags as c::c_long,
+            c::c_long::from(oflags),
             mode as c::c_long,
         ) as c::c_int)
     }
@@ -707,7 +707,7 @@ pub(crate) fn mknodat(
             borrowed_fd(dirfd),
             c_str(path),
             (mode.bits() | file_type.as_raw_mode()) as c::mode_t,
-            dev.try_into().map_err(|_| io::Error::PERM)?,
+            dev.try_into().map_err(|_e| io::Error::PERM)?,
         ))
     }
 }

src/imp/libc/net/syscalls.rs
@@ -376,7 +376,7 @@ pub(crate) fn socketpair(
     unsafe {
         let mut fds = MaybeUninit::<[OwnedFd; 2]>::uninit();
         ret(c::socketpair(
-            domain.0 as c::c_int,
+            c::c_int::from(domain.0),
             type_.0 as c::c_int | flags.bits(),
             protocol.0,
             fds.as_mut_ptr().cast::<c::c_int>(),
@@ -501,7 +501,7 @@ pub(crate) mod sockopt {
             if linger.subsec_nanos() != 0 {
                 l_linger = l_linger.checked_add(1).ok_or(io::Error::INVAL)?;
             }
-            l_linger.try_into().map_err(|_| io::Error::INVAL)?
+            l_linger.try_into().map_err(|_e| io::Error::INVAL)?
         } else {
             0
         };

src/imp/libc/process/syscalls.rs
@@ -326,12 +326,13 @@ fn rlimit_from_libc(lim: libc_rlimit) -> Rlimit {
 /// Convert a C `libc_rlimit` to a Rust `Rlimit`.
 #[cfg(not(any(target_os = "fuchsia", target_os = "redox", target_os = "wasi")))]
 fn rlimit_to_libc(lim: Rlimit) -> io::Result<libc_rlimit> {
-    let rlim_cur = match lim.current {
-        Some(r) => r.try_into().map_err(|_| io::Error::INVAL)?,
+    let Rlimit { current, maximum } = lim;
+    let rlim_cur = match current {
+        Some(r) => r.try_into().map_err(|_e| io::Error::INVAL)?,
         None => LIBC_RLIM_INFINITY as _,
     };
-    let rlim_max = match lim.maximum {
-        Some(r) => r.try_into().map_err(|_| io::Error::INVAL)?,
+    let rlim_max = match maximum {
+        Some(r) => r.try_into().map_err(|_e| io::Error::INVAL)?,
         None => LIBC_RLIM_INFINITY as _,
     };
     Ok(libc_rlimit { rlim_cur, rlim_max })

src/imp/linux_raw/net/syscalls.rs
@@ -901,12 +901,12 @@ pub(crate) mod sockopt {
             if linger.subsec_nanos() != 0 {
                 l_linger = l_linger.checked_add(1).ok_or(io::Error::INVAL)?;
             }
-            l_linger.try_into().map_err(|_| io::Error::INVAL)?
+            l_linger.try_into().map_err(|_e| io::Error::INVAL)?
         } else {
             0
         };
         let linger = c::linger {
-            l_onoff: linger.is_some() as c::c_int,
+            l_onoff: c::c_int::from(linger.is_some()),
             l_linger,
         };
         setsockopt(fd, c::SOL_SOCKET as _, c::SO_LINGER, linger)
@@ -1030,7 +1030,7 @@ pub(crate) mod sockopt {
                 }
                 let mut timeout = __kernel_timespec {
                     tv_sec: timeout.as_secs().try_into().unwrap_or(i64::MAX),
-                    tv_nsec: timeout.subsec_nanos() as _,
+                    tv_nsec: timeout.subsec_nanos().into(),
                 };
                 if timeout.tv_sec == 0 && timeout.tv_nsec == 0 {
                     timeout.tv_nsec = 1;
@@ -1225,7 +1225,7 @@ pub(crate) mod sockopt {
 
     #[inline]
     fn from_bool(value: bool) -> c::c_uint {
-        value as c::c_uint
+        c::c_uint::from(value)
     }
 
     #[inline]

src/imp/linux_raw/process/syscalls.rs
@@ -416,6 +416,7 @@ fn rlimit_to_linux(lim: Rlimit) -> io::Result<linux_raw_sys::general::rlimit64> 
 }
 
 /// Like `rlimit_from_linux` but uses Linux's old 32-bit `rlimit`.
+#[allow(clippy::useless_conversion)]
 fn rlimit_from_linux_old(lim: linux_raw_sys::general::rlimit) -> Rlimit {
     let current = if lim.rlim_cur == linux_raw_sys::general::RLIM_INFINITY as _ {
         None
@@ -431,13 +432,14 @@ fn rlimit_from_linux_old(lim: linux_raw_sys::general::rlimit) -> Rlimit {
 }
 
 /// Like `rlimit_to_linux` but uses Linux's old 32-bit `rlimit`.
+#[allow(clippy::useless_conversion)]
 fn rlimit_to_linux_old(lim: Rlimit) -> io::Result<linux_raw_sys::general::rlimit> {
     let rlim_cur = match lim.current {
-        Some(r) => r.try_into().map_err(|_| io::Error::INVAL)?,
+        Some(r) => r.try_into().map_err(|_e| io::Error::INVAL)?,
         None => linux_raw_sys::general::RLIM_INFINITY as _,
     };
     let rlim_max = match lim.maximum {
-        Some(r) => r.try_into().map_err(|_| io::Error::INVAL)?,
+        Some(r) => r.try_into().map_err(|_e| io::Error::INVAL)?,
         None => linux_raw_sys::general::RLIM_INFINITY as _,
     };
     Ok(linux_raw_sys::general::rlimit { rlim_cur, rlim_max })

openharmony/third_party_rust_rustix

src/imp/libc/fs/syscalls.rs
@@ -164,9 +164,9 @@ fn openat_via_syscall(
         let mode = c::c_uint::from(mode.bits());
         ret_owned_fd(c::syscall(
             c::SYS_openat,
-            dirfd as c::c_long,
+            c::c_long::from(dirfd),
             path,
-            oflags as c::c_long,
+            c::c_long::from(oflags),
             mode as c::c_long,
         ) as c::c_int)
     }
@@ -707,7 +707,7 @@ pub(crate) fn mknodat(
             borrowed_fd(dirfd),
             c_str(path),
             (mode.bits() | file_type.as_raw_mode()) as c::mode_t,
-            dev.try_into().map_err(|_| io::Errno::PERM)?,
+            dev.try_into().map_err(|_e| io::Errno::PERM)?,
         ))
     }
 }

src/imp/libc/net/syscalls.rs
@@ -376,7 +376,7 @@ pub(crate) fn socketpair(
     unsafe {
         let mut fds = MaybeUninit::<[OwnedFd; 2]>::uninit();
         ret(c::socketpair(
-            domain.0 as c::c_int,
+            c::c_int::from(domain.0),
             type_.0 as c::c_int | flags.bits(),
             protocol.0,
             fds.as_mut_ptr().cast::<c::c_int>(),
@@ -501,7 +501,7 @@ pub(crate) mod sockopt {
             if linger.subsec_nanos() != 0 {
                 l_linger = l_linger.checked_add(1).ok_or(io::Errno::INVAL)?;
             }
-            l_linger.try_into().map_err(|_| io::Errno::INVAL)?
+            l_linger.try_into().map_err(|_e| io::Errno::INVAL)?
         } else {
             0
         };

src/imp/libc/process/syscalls.rs
@@ -326,12 +326,13 @@ fn rlimit_from_libc(lim: libc_rlimit) -> Rlimit {
 /// Convert a C `libc_rlimit` to a Rust `Rlimit`.
 #[cfg(not(any(target_os = "fuchsia", target_os = "redox", target_os = "wasi")))]
 fn rlimit_to_libc(lim: Rlimit) -> io::Result<libc_rlimit> {
-    let rlim_cur = match lim.current {
-        Some(r) => r.try_into().map_err(|_| io::Errno::INVAL)?,
+    let Rlimit { current, maximum } = lim;
+    let rlim_cur = match current {
+        Some(r) => r.try_into().map_err(|_e| io::Errno::INVAL)?,
         None => LIBC_RLIM_INFINITY as _,
     };
-    let rlim_max = match lim.maximum {
-        Some(r) => r.try_into().map_err(|_| io::Errno::INVAL)?,
+    let rlim_max = match maximum {
+        Some(r) => r.try_into().map_err(|_e| io::Errno::INVAL)?,
         None => LIBC_RLIM_INFINITY as _,
     };
     Ok(libc_rlimit { rlim_cur, rlim_max })

src/imp/linux_raw/net/syscalls.rs
@@ -901,12 +901,12 @@ pub(crate) mod sockopt {
             if linger.subsec_nanos() != 0 {
                 l_linger = l_linger.checked_add(1).ok_or(io::Errno::INVAL)?;
             }
-            l_linger.try_into().map_err(|_| io::Errno::INVAL)?
+            l_linger.try_into().map_err(|_e| io::Errno::INVAL)?
         } else {
             0
         };
         let linger = c::linger {
-            l_onoff: linger.is_some() as c::c_int,
+            l_onoff: c::c_int::from(linger.is_some()),
             l_linger,
         };
         setsockopt(fd, c::SOL_SOCKET as _, c::SO_LINGER, linger)
@@ -1030,7 +1030,7 @@ pub(crate) mod sockopt {
                 }
                 let mut timeout = __kernel_timespec {
                     tv_sec: timeout.as_secs().try_into().unwrap_or(i64::MAX),
-                    tv_nsec: timeout.subsec_nanos() as _,
+                    tv_nsec: timeout.subsec_nanos().into(),
                 };
                 if timeout.tv_sec == 0 && timeout.tv_nsec == 0 {
                     timeout.tv_nsec = 1;
@@ -1225,7 +1225,7 @@ pub(crate) mod sockopt {
 
     #[inline]
     fn from_bool(value: bool) -> c::c_uint {
-        value as c::c_uint
+        c::c_uint::from(value)
     }
 
     #[inline]

src/imp/linux_raw/process/syscalls.rs
@@ -416,6 +416,7 @@ fn rlimit_to_linux(lim: Rlimit) -> io::Result<linux_raw_sys::general::rlimit64> 
 }
 
 /// Like `rlimit_from_linux` but uses Linux's old 32-bit `rlimit`.
+#[allow(clippy::useless_conversion)]
 fn rlimit_from_linux_old(lim: linux_raw_sys::general::rlimit) -> Rlimit {
     let current = if lim.rlim_cur == linux_raw_sys::general::RLIM_INFINITY as _ {
         None
@@ -431,13 +432,14 @@ fn rlimit_from_linux_old(lim: linux_raw_sys::general::rlimit) -> Rlimit {
 }
 
 /// Like `rlimit_to_linux` but uses Linux's old 32-bit `rlimit`.
+#[allow(clippy::useless_conversion)]
 fn rlimit_to_linux_old(lim: Rlimit) -> io::Result<linux_raw_sys::general::rlimit> {
     let rlim_cur = match lim.current {
-        Some(r) => r.try_into().map_err(|_| io::Errno::INVAL)?,
+        Some(r) => r.try_into().map_err(|_e| io::Errno::INVAL)?,
         None => linux_raw_sys::general::RLIM_INFINITY as _,
     };
     let rlim_max = match lim.maximum {
-        Some(r) => r.try_into().map_err(|_| io::Errno::INVAL)?,
+        Some(r) => r.try_into().map_err(|_e| io::Errno::INVAL)?,
         None => linux_raw_sys::general::RLIM_INFINITY as _,
     };
     Ok(linux_raw_sys::general::rlimit { rlim_cur, rlim_max })