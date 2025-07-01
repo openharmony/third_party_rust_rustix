5d9c62cc106208ff0bb8068b3ec952938a913e10
ljy9810 <longjianyin@h-partners.com>
2025-07-01 19:30:21 +0800

    rustix 0.38.8 社区漏洞 CVE-2024-43806 修复 修复措施：升级rustix版本到0.38.16版本
    Signed-off-by: ljy9810 <longjianyin@h-partners.com>

--- tests/backends.rs
+++ tests/backends.rs
@@ -2,6 +2,13 @@ use std::process::Command;
 
 #[test]
 fn test_backends() {
+    // Test whether `has_dependency` works. `cargo tree` no longer works in
+    // Rust 1.48 because `cargo tree` pulls in dependencies for all targets,
+    // and hermit-core isn't compatible with Rust 1.48.
+    if !has_dependency(".", &[], &[], &[], "io-lifetimes") {
+        return;
+    }
+
     // Pick an arbitrary platform where linux_raw is enabled by default and
     // ensure that the use-default crate uses it.
     #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
@@ -51,8 +58,17 @@ fn test_backends() {
 
     #[cfg(windows)]
     let libc_dep = "windows-sys";
-    #[cfg(unix)]
+    #[cfg(any(unix, target_os = "wasi"))]
     let libc_dep = "libc";
+
+    // FIXME: Temporarily disable the subsequent tests on Windows until
+    // rust-errno updates to windows-sys 0.48.
+    #[cfg(windows)]
+    {
+        if true {
+            return;
+        }
+    }
 
     // Test the use-libc crate, which enables the "use-libc" cargo feature.
     assert!(
